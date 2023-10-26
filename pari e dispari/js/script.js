43512bbd357607a7c368ba083f4c1ee0161af8e3
riccardococciolo <coccioloriccardo64@gmail.com>
2023-10-26 17:02:21 +0200

    add: random number

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,4 +1,15 @@
-const userChoise = parseInt(prompt("Scegli pari o dispari"));
+const userChoise = (prompt("Scegli pari o dispari"));
 const userNumber = parseInt(prompt("Scegli un nemero da 1 a 5"));
 console.log(userChoise, userNumber)
 
+function generateRandomNumber(min, max) {
+    const randomNumber = Math.floor(Math.random() * (max - min) + min);
+    return randomNumber;
+}
+
+console.log(generateRandomNumber(1, 5));
+
+const pcNumber = generateRandomNumber(1, 5);
+
+const sum = pcNumber + userNumber;
+console.log(sum);
